08f3e5b0387217bd1feedd5ef94e94041cc4dc91
Sofiane Elguendouze <fs_elguendouze@esi.dz>
2018-12-28 00:52:16 +0100

    Some changes in hero dashboard and hero list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,19 +8,11 @@ import { HeroService } from '../hero.service'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero : Hero = { id: 1, name: 'windstrom'};
   heroes : Hero[];
-  selectedHero : Hero;
-  onSelect(hero:Hero) : void {
-    this.selectedHero = hero;
-  }
-  constructor(private heroService : HeroService) { 
-
-  }
+  constructor(private heroService : HeroService) {}
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
-
   ngOnInit() {
     this.getHeroes();
   }
