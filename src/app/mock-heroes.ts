aec427f58360d98f1e33ea6717b9c63d9145bc90
Sofiane Elguendouze <fs_elguendouze@esi.dz>
2019-01-17 00:34:39 +0100

    Add some Heroes

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,7 +1,7 @@
 import { Hero } from './hero';
 
 export const HEROES : Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
+  { id: 11, name: 'Mohammed' },
   { id: 12, name: 'Narco' },
   { id: 13, name: 'Bombasto' },
   { id: 14, name: 'Celeritas' },
@@ -11,5 +11,8 @@ export const HEROES : Hero[] = [
   { id: 18, name: 'Dr IQ' },
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' },
-  { id: 21, name: 'SOFIANE' }
+  { id: 21, name: 'SOFIANE' },
+  { id: 22, name: 'Salah' },
+  { id: 23, name: 'Kamal' },
+  { id: 24, name: 'Amir' }
 ];
