97f2c3d42da63d8bc50964e87c830220a70f403d
Sofiane Elguendouze <fs_elguendouze@esi.dz>
2019-01-18 23:54:17 +0100

    Clean heroes mock

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -13,8 +13,5 @@ export const HEROES : Hero[] = [
   { id: 20, name: 'Tornado' },
   { id: 21, name: 'SOFIANE' },
   { id: 22, name: 'Salah' },
-  { id: 23, name: 'Kamal' },
-  { id: 24, name: 'Amir' },
-  { id: 24, name: 'Amir1' },
-  { id: 24, name: 'Amir2' }
+  { id: 23, name: 'Kamal' }
 ];
